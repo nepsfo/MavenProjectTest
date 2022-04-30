eb01fa32f12d1013cbc44296560dd394f8799061
nepsfo <nepsfo@gmail.com>
2022-04-30 08:20:33 -0700

    First Checkout 04302022

---
+++ src/test/java/com/nike/MavenProjectTest/AppTest.java
@@ -0,0 +1,21 @@
+package com.nike.MavenProjectTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        System.out.println("Success One Two Three");
+    }
+}
